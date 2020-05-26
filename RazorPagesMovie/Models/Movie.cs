39e027413e345ccdd2d2dd4ae0425cff0005af89
Marek Słomnicki <mslomnicki@bravurasolutions.com>
2020-05-26 18:20:43 +0200

    RazorPages tutorial

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Wrong length dude"), Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public string Rating { get; set; }
+    }
+}
